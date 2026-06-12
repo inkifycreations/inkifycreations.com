3463fc171e8dc5dc294ac7ee88182d38f31c1f31
inkifycreations <joshuaporapu14111999@gmail.com>
2026-06-13 01:36:17 +0530

    Configure postgres database support

--- backend/inkify_backend/settings.py
+++ backend/inkify_backend/settings.py
@@ -86,12 +86,24 @@ WSGI_APPLICATION = 'inkify_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if os.getenv('USE_POSTGRES', 'False').lower() == 'true':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'inkify_db',
+            'USER': 'inkify_user',
+            'PASSWORD': 'your_strong_password',
+            'HOST': 'localhost',
+            'PORT': '5432',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
